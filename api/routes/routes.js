d8bdc7635383ea6818b8f9b11226d8e0e23efb1a
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-11-09 07:47:57 +0000

    change var to const

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(app) {
-var con = require('../controllers/controller');
+const con = require('../controllers/controller');
 
 //GET
 app.route('/').get(con.home);
