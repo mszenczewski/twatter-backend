96024660d8c4d132cedebb12f59020c1fcc83f12
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-11-09 07:48:10 +0000

    move logger stuff to logger file

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -6,43 +6,7 @@ const mongoose = require('mongoose');
 const User = mongoose.model('Users');
 const Item = mongoose.model('Items');
 
-const { createLogger, format, transports } = require('winston');
-const filesystem = require('fs');
-const log_dir = 'log';
-require('winston-daily-rotate-file');
-
-if (!filesystem.existsSync(log_dir)) {
-  filesystem.mkdirSync(log_dir);
-}
-
-const daily_rotate_file_transport = new transports.DailyRotateFile
-({
-  filename: `${log_dir}/%DATE%-results.log`,
-  datePattern: 'YYYY-MM-DD'
-});
-
-const logger = createLogger({
-  level: 'DEBUG',
-  levels: { 
-    ERROR: 0,
-    WARN: 1,
-    INFO: 2,
-    DEBUG: 3
-  },
-  format: format.combine(
-    format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-  ),
-  transports: [
-    new transports.Console({
-      format: format.combine(
-        format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-      ),
-    }),
-    daily_rotate_file_transport
-  ]
-});
+const logger = require('./logger');
 
 /**
  * HOME
---
+++ api/controllers/logger.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { createLogger, format, transports } = require('winston');
+const filesystem = require('fs');
+const log_dir = 'log';
+require('winston-daily-rotate-file');
+
+if (!filesystem.existsSync(log_dir)) {
+  filesystem.mkdirSync(log_dir);
+}
+
+const daily_rotate_file_transport = new transports.DailyRotateFile
+({
+  filename: `${log_dir}/%DATE%-results.log`,
+  datePattern: 'YYYY-MM-DD'
+});
+
+module.exports = createLogger({
+  level: 'DEBUG',
+  levels: { 
+    ERROR: 0,
+    WARN: 1,
+    INFO: 2,
+    DEBUG: 3
+  },
+  format: format.combine(
+    format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+        format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+      ),
+    }),
+    daily_rotate_file_transport
+  ]
+});
