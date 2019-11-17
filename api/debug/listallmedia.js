cdccd576e75d97f7870d5d910d382dbbd38f82ca
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-11-17 05:42:22 +0000

    remove buffer from output

--- api/debug/listallmedia.js
+++ api/debug/listallmedia.js
@@ -8,13 +8,17 @@ const mongoose = require('mongoose'),
  * LIST ALL MEDIA 
  * Returns all media in the database
  */
-module.exports = function(req, res) {
-  Media.find({}, function(err, media) {
-    if (err) {
-      logger.ERROR('[LISTALLMEDIA] ' + err);
-      res.json({status: 'error', error: 'fatal'});
-      return;
+module.exports = async function(req, res) {
+  try {
+    const results = await Media.find({});
+
+    for (var i = 0; i < results.length; i++) {
+      results[i].content.data = null;
     }
-    res.json(media);
-  });
+
+    res.json(results);
+  } catch (err) {
+    logger.ERROR('[LISTALLMEDIA] ' + err);
+    res.json({status: 'error', error: 'fatal'});
+  }
 };
