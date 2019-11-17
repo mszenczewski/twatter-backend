f598005209fde4f84232775f1825dea567a1d0cf
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2019-11-17 23:49:32 +0000

    refactor verify and add status codes

--- api/controllers/verify.js
+++ api/controllers/verify.js
@@ -10,50 +10,45 @@ const User = mongoose.model('Users');
  * Verifies email with randomly generated key
  * JSON: { email:, key: }
  */
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
   logger.DEBUG('[VERIFY] received: ' + JSON.stringify(req.body));
 
   if (req.body.email === '') {
     logger.WARN('[VERIFY] no email entered');
-    res.json({status: 'error', error: 'no email entered'});
+    res.status(400).json({status: 'error', error: 'no email entered'});
     return;
   }
 
   if (req.body.key === '') {
     logger.WARN('[VERIFY] no key entered');
-    res.json({status: 'error', error: 'no key entered'});
+    res.status(400).json({status: 'error', error: 'no key entered'});
     return;
   }
 
-  User.findOne({email: req.body.email}, function(err, user) {
-    if (err) {
-      logger.ERROR('[VERIFY] ' + err);
-      res.json({status: 'error', error: 'fatal'});
-      return;
-    }
+  try {
+    const user = await User.findOne({email: req.body.email});
 
     if(user === null) {
       logger.WARN('[VERIFY] user not found');
-      res.json({status: 'error', error: 'user not found'});
+      res.status(404).json({status: 'error', error: 'user not found'});
       return;
     }
 
     if(user.key !== req.body.key && req.body.key !== 'abracadabra'){
       logger.WARN('[VERIFY] incorrect key');
-      res.json({status: 'error', error: 'incorrect key'});
+      res.status(403).json({status: 'error', error: 'incorrect key'});
       return;
     }
 
     user.verified = true;
 
-    user.save(function(err, user) {
-      if (err) {
-        logger.ERROR('[VERIFY] ' + err);
-        res.json({status: 'error', error: 'fatal'});
-        return;
-      }
-      logger.INFO('[VERIFY] ' + user.email + ' verified');
-      res.json({status: 'OK'});
-    });
-  });
+    await user.save();
+
+    logger.INFO('[VERIFY] ' + user.email + ' verified');
+    res.json({status: 'OK'});
+
+  } catch (err) {
+    logger.ERROR('[VERIFY] ' + err);
+    res.status(500).json({status: 'error', error: 'fatal'}); 
+  }
 };
