b7bdf716deb4e2105efd5709d4947531bbd764ed
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2024-02-09 13:14:28 -0500

    framework for testing

---
+++ api/test/test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('test group', () => {
+    it('test one', () => {
+        assert.isTrue(true);
+    });
+
+    it('test two', () => {
+        assert.isTrue(true);
+    });
+});
