f5e84c1a27ae93e4463df510178b6794d30ac53b
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2024-02-12 11:53:06 -0500

    clean up logger

--- api/logger.js
+++ api/logger.js
@@ -7,17 +7,15 @@ import get_config from '../get_config.js';
 
 const log_dir = 'log';
 
-if (!fs.existsSync(log_dir)) {
-  fs.mkdirSync(log_dir);
-}
+if (!fs.existsSync(log_dir)) fs.mkdirSync(log_dir);
 
-const daily_rotate_file_transport = new transports.DailyRotateFile
-({
+const daily_rotate_file = new transports.DailyRotateFile({
   filename: `${log_dir}/%DATE%-results.log`,
   datePattern: 'YYYY-MM-DD'
 });
 
 const cfg = get_config();
+const timestamp_format = {format: 'YYYY-MM-DD HH:mm:ss'};
 
 const logger = createLogger({
   level: cfg.log_level,
@@ -28,23 +26,24 @@ const logger = createLogger({
     debug: 3
   },
   format: format.combine(
-      format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-      format.printf(x => `${x.timestamp} ${x.level.toUpperCase()}: [${x.label.toUpperCase()}] ${x.message}`)
+      format.timestamp(timestamp_format),
+      format.printf(msg => format_msg(msg))
   ),
   transports: [
     new transports.Console({
       format: format.combine(
-          format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-          format.printf(x => `${x.timestamp} ${x.level.toUpperCase()}: [${x.label.toUpperCase()}] ${x.message}`)
+          format.timestamp(timestamp_format),
+          format.printf(msg => format_msg(msg))
       ),
     }),
-    daily_rotate_file_transport
+    daily_rotate_file
   ]
 });
+
+function format_msg(x) {
+  return `${x.timestamp} ${x.level.toUpperCase()}: [${x.label.toUpperCase()}] ${x.message}`;
+}
 
 export default function logger_child(label){
   return logger.child({label: label});
 }
-
-
-
