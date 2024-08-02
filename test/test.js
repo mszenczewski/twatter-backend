c06e3bdb3984cccb13eeb462407afc63a3337b08
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2024-08-02 16:21:11 -0400

    clean up comments

--- test/test.js
+++ test/test.js
@@ -6,24 +6,27 @@ chai.use(chaiHttp);
 
 describe('user tests', () => {
     it('register user', async () => {
-        let res  = await chai.request(server).post('/reset');
-        let text = JSON.parse(res.text);
-
-        assert.equal(200, res.statusCode);
-        assert.equal('OK', text.status);
-
         const json = {
             username: 'test_user',
             password: 'test_password',
             email: 'testemail@testemail.com'
         };
 
+        //reset database
+        let res  = await chai.request(server).post('/reset');
+        let text = JSON.parse(res.text);
+
+        assert.equal(200, res.statusCode);
+        assert.equal('OK', text.status);
+
+        //add user
         res = await chai.request(server).post('/adduser').send(json);
         text = JSON.parse(res.text);
 
         assert.equal(200, res.statusCode);
         assert.equal('OK', text.status);
 
+        //attempt to add user again
         res = await chai.request(server).post('/adduser').send(json);
         text = JSON.parse(res.text);
 
@@ -32,6 +35,5 @@ describe('user tests', () => {
         assert.equal('user already exists', text.error);
 
         //TODO verify the user was added
-
     });
 });
