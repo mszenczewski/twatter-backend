2b19049b4a59c9ad862b172544ec93ea11af2749
Michael Szenczewski <12867533+mszenczewski@users.noreply.github.com>
2024-08-03 12:24:25 -0400

    clean up testing structure

--- test/test.js
+++ test/test.js
@@ -4,102 +4,124 @@ import server from '../server.js';
 
 chai.use(chaiHttp);
 
-describe('user tests', () => {
-    let res;
-    let text;
-    const user_0 = {
-        username: 'user_0',
-        password: 'password_0',
-        email: 'email_0@example.com'
-    };
-    const user_1 = {
-        username: 'user_1',
-        password: 'password_1',
-        email: 'email_1@example.com'
-    };
+async function send_post(address, expected_status_code, json) {
+    const response = await chai.request(server).post(address).send(json);
+    assert.equal(expected_status_code, response.statusCode);
+    return JSON.parse(response.text);
+}
 
-    it('removing users from database', async () => {
+async function send_get(address, expected_status_code) {
+    const response = await chai.request(server).get(address);
+    assert.equal(expected_status_code, response.statusCode);
+    return JSON.parse(response.text);
+}
+
+async function send_delete(address, expected_status_code) {
+    const response = await chai.request(server).delete(address);
+    assert.equal(expected_status_code, response.statusCode);
+    return JSON.parse(response.text);
+}
+
+let text;
+const user_0 = {
+    username: 'user_0',
+    password: 'password_0',
+    email: 'email_0@example.com'
+};
+const user_1 = {
+    username: 'user_1',
+    password: 'password_1',
+    email: 'email_1@example.com'
+};
+
+describe('admin tests', () => {
+    it('reset database', async () => {
+        //reset database
+        text = await send_delete('/reset', 200);
+        assert.equal('OK', text.status);
+
+        //get item list
+        text = await send_get('/listallitems', 200);
+        assert.isArray(text);
+        assert.isEmpty(text);
+
+        //get media list
+        text = await send_get('/listallmedia', 200);
+        assert.isArray(text);
+        assert.isEmpty(text);
+
+        //get user list
+        text = await send_get('/listallusers', 200);
+        assert.isArray(text);
+        assert.isEmpty(text);
+    });
+    it('remove all users', async () => {
         //add user_0
-        res = await chai.request(server).post('/adduser').send(user_0);
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        text = await send_post('/adduser', 200, user_0);
         assert.equal('OK', text.status);
 
         //add user_1
-        res = await chai.request(server).post('/adduser').send(user_1);
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        text = await send_post('/adduser', 200, user_1);
         assert.equal('OK', text.status);
+
+        //list all users
+        text = await send_get('/listallusers', 200);
+        assert.isArray(text);
+        assert.lengthOf(text, 2);
+        assert.equal(text[0].username, user_0.username);
+        assert.equal(text[0].password, user_0.password);
+        assert.equal(text[0].email, user_0.email);
+        assert.equal(text[1].username, user_1.username);
+        assert.equal(text[1].password, user_1.password);
+        assert.equal(text[1].email, user_1.email);
 
         //remove all users
-        res  = await chai.request(server).post('/removeallusers');
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        text = await send_delete('/removeallusers', 200);
         assert.equal('OK', text.status);
 
-        //attempt to get user_0
-        res = await chai.request(server).get(`/user/${user_0.username}`);
-        text = JSON.parse(res.text);
-        assert.equal(404, res.statusCode);
-        assert.equal('error', text.status);
-        assert.equal('user not found', text.error);
-
-        //attempt to get user_1
-        res = await chai.request(server).get(`/user/${user_1.username}`);
-        text = JSON.parse(res.text);
-        assert.equal(404, res.statusCode);
-        assert.equal('error', text.status);
-        assert.equal('user not found', text.error);
-
-        //attempt to list all users
-        res = await chai.request(server).post('/listallitems');
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
-        assert.isArray(JSON.parse(res.text));
-        assert.isEmpty(JSON.parse(res.text));
+        //list all users
+        text = await send_get('/listallusers', 200);
+        assert.isArray(text);
+        assert.isEmpty(text);
     });
+});
 
+describe('user tests', () => {
     it('add user', async () => {
-        //reset database
-        res  = await chai.request(server).post('/removeallusers');
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        //add user_0
+        text = await send_post('/adduser', 200, user_0);
         assert.equal('OK', text.status);
 
-        //add user
-        res = await chai.request(server).post('/adduser').send(user_0);
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        //add user_1
+        text = await send_post('/adduser', 200, user_1);
         assert.equal('OK', text.status);
 
-        //attempt to add user again
-        res = await chai.request(server).post('/adduser').send(user_0);
-        text = JSON.parse(res.text);
-        assert.equal(400, res.statusCode);
+        //add user_0 again
+        text = await send_post('/adduser', 400, user_0);
         assert.equal('error', text.status);
         assert.equal('user already exists', text.error);
 
-        //attempt to get user
-        res = await chai.request(server).get(`/user/${user_0.username}`);
-        text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        //add user_1 again
+        text = await send_post('/adduser', 400, user_1);
+        assert.equal('error', text.status);
+        assert.equal('user already exists', text.error);
+
+        //attempt to get user_0
+        text = await send_get(`/user/${user_0.username}`, 200);
         assert.equal('OK', text.status);
         assert.equal(user_0.email, text.user.email);
         assert.isArray(text.user.following);
         assert.isArray(text.user.followers);
         assert.isEmpty(text.user.following);
         assert.isEmpty(text.user.followers);
-    });
-});
 
-describe('admin tests', () => {
-    it('reset database', async () => {
-        //reset database
-        let res  = await chai.request(server).post('/reset');
-        let text = JSON.parse(res.text);
-        assert.equal(200, res.statusCode);
+        //attempt to get user_1
+        text = await send_get(`/user/${user_1.username}`, 200);
         assert.equal('OK', text.status);
-
-        //TODO check that all the items are empty
+        assert.equal(user_1.email, text.user.email);
+        assert.isArray(text.user.following);
+        assert.isArray(text.user.followers);
+        assert.isEmpty(text.user.following);
+        assert.isEmpty(text.user.followers);
     });
 });
